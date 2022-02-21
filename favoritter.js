302f4fd4aa014c4968753aa3c9748af8f8697c99
”Jonathan <jona6893@stud.kea.dkgit config --global user.name Jonathan Weldongit config --global user.email jona6893@stud.kea.dk>
2022-02-21 22:17:10 +0100

    men kan nu fjerne ting fra favoritter

--- favoritter.js
+++ favoritter.js
@@ -1,24 +1,39 @@
 /* let produkt = sessionStorage.getItem("favorit"); */
+//henter vores string, som tidligere var en Array. Den laves derefter om til en Array igen.
+let favoritter = JSON.parse(sessionStorage.getItem("favoritter"));
+const spand = document.querySelectorAll(".spand");
 
-let produkter = JSON.parse(sessionStorage.getItem("favoritter"));
+console.log(favoritter);
 
-console.log(produkter);
-
-function printud(produkter) {
+function printud(favoritter) {
   const data = document.querySelector(".data_products");
   const tøjTemplate = document.querySelector("template");
   data.textContent = "";
 
-  produkter.forEach((item) => {
+  favoritter.forEach((item) => {
     console.log(item);
     const klon = tøjTemplate.cloneNode(true).content;
     klon.querySelector("h3").textContent = item.navn;
     klon.querySelector("img").src = item.billede1;
-
     klon.querySelector("p").textContent =
       item.pris1 + "kr. " + item.pris2 + "kr.";
+      //når spanden klikkes, start annonym(arrow funktion) funktion
+    klon.querySelector(".spand").addEventListener("click", () => {
+      // finder id'et af den valgte spand
+      let id = favoritter.indexOf(item)
+      console.log(id)
+      //fjerner alt imellem vores valgt id os så en index frem. Altså kun den valgte spand fjernes fra listen.
+      favoritter.splice(id, 1);
+      console.log(favoritter);
+      //Tilføjer vores array favoritter til sessionStorage
+      sessionStorage.setItem("favoritter", JSON.stringify(favoritter));
+      // reloader vores side for de viste produkter bliver opdateret.
+      window.location.reload();
+    });
     data.appendChild(klon);
   });
 }
 
-printud(produkter);
+
+
+printud(favoritter);
