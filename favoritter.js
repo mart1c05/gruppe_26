ee9bb94c4cbc160f38948e86783ba4c0086e7be0
”Jonathan <jona6893@stud.kea.dkgit config --global user.name Jonathan Weldongit config --global user.email jona6893@stud.kea.dk>
2022-02-22 11:04:08 +0100

    sorting

--- favoritter.js
+++ favoritter.js
@@ -1,41 +1,64 @@
 /* let produkt = sessionStorage.getItem("favorit"); */
 //henter vores string, som tidligere var en Array. Den laves derefter om til en Array igen.
-let favorit = JSON.parse(sessionStorage.getItem("favorit"));
+let favoritter = JSON.parse(sessionStorage.getItem("favoritter"));
 const spand = document.querySelectorAll(".spand");
+const foldbnt = document.querySelector(".foldout");
+const filterKnapper = document.querySelectorAll("nav button");
+let filter = "alle";
 
+console.log(favoritter);
 
-console.log(favorit);
+  
+filterKnapper.forEach((knap) => knap.addEventListener("click", filtrerTøj));
 
-function printud(favorit) {
-  const data = document.querySelector(".data_products");
-  const tøjTemplate = document.querySelector("template");
-  data.textContent = "";
+ function filtrerTøj() {
+  filter = this.dataset.kategori;
+  document.querySelector(".valgt").classList.remove("valgt");
+  this.classList.add("valgt");
+  console.log(filter);
+  printud(favoritter);
 
-  favorit.forEach((item) => {
-    console.log(item);
-    const klon = tøjTemplate.cloneNode(true).content;
-    klon.querySelector("h3").textContent = item.navn;
-    klon.querySelector("img").src = item.billede1;
-    klon.querySelector("p").textContent =
-      item.pris1 + "kr. " + item.pris2 + "kr.";
-      //når spanden klikkes, start annonym(arrow funktion) funktion
-    klon.querySelector(".spand").addEventListener("click", () => {
-      // finder id'et af den valgte spand
-      let id = favorit.indexOf(item)
-      console.log(id)
-      //fjerner alt imellem vores valgt id os så en index frem. Altså kun den valgte spand fjernes fra listen.
-      favorit.splice(id, 1);
-      console.log(favorit);
-      //Tilføjer vores array favorit til sessionStorage
-      sessionStorage.setItem("favorit", JSON.stringify(favorit));
-      // reloader vores side for de viste produkter bliver opdateret.
-      window.location.reload();
-    });
-    data.appendChild(klon);
-  });
 }
 
 
 
-printud(favorit);
+function printud(favoritter) {
+  const data = document.querySelector(".data_products");
+  const tøjTemplate = document.querySelector("template");
+  data.textContent = "";
+
+  favoritter.forEach((item) => {
+    console.log(item);
+    if (filter == item.kategori || filter == "alle") {
+      const klon = tøjTemplate.cloneNode(true).content;
+      klon.querySelector("h3").textContent = item.navn;
+      klon.querySelector("img").src = item.billede1;
+      klon.querySelector("p").textContent =
+        item.pris1 + "kr. " + item.pris2 + "kr.";
+      //når spanden klikkes, start annonym(arrow funktion) funktion
+      klon.querySelector(".spand").addEventListener("click", () => {
+        // finder id'et af den valgte spand
+        let id = favoritter.indexOf(item);
+        console.log(id);
+        //fjerner alt imellem vores valgt id os så en index frem. Altså kun den valgte spand fjernes fra listen.
+        favoritter.splice(id, 1);
+        console.log(favoritter);
+        //Tilføjer vores array favorit til sessionStorage
+        sessionStorage.setItem("favoritter", JSON.stringify(favoritter));
+        // reloader vores side for de viste produkter bliver opdateret.
+        window.location.reload();
+      });
+      data.appendChild(klon);
+    }
+  });
+}
+
+// toggler klassen none på knappen filter
+function foldOut() {
+  console.log("hi");
+  foldbnt.classList.toggle("none");
+}
+
+
+printud(favoritter);
 
